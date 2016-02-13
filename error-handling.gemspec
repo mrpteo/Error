bb55f98d2cee55676071422a2644e7d73d6cc25f
mrpteo <peterarnell12@gmail.com>
2016-02-13 16:36:08 +0000

    Latest Version
    Updated, Improved... Cleaner!

--- error-handling.gemspec
+++ error-handling.gemspec
@@ -1,13 +1,13 @@
 Gem::Specification.new do |gem_info|
 	gem_info.name = "error-handling"
-	gem_info.version = "1.3.1"
+	gem_info.version = "1.4"
 	gem_info.default_executable = "ErrorHandling"
 
 	gem_info.authors = ["Peter Arnell"]
-	gem_info.date = %q{2015-01-24}
-	gem_info.summary     = "Beautiful solution to error handling."
-	gem_info.description = %q{Error-Handling is a gem focused purly on improving how we handle errors in our applications. Using a number of methods it allows you to create, export, display and much more.}
+	gem_info.date = %q{2016-02-12}
+	gem_info.summary     = "Beautiful solution to application error handling."
+	gem_info.description = %q{Error-Handling is a gem focused on improving how we handle errors in our applications. Using a number of methods it allows you to create, export, display and much more.}
 	gem_info.files = ["lib/error-handling.rb"]
 	gem_info.license       = 'MIT'
-	gem_info.homepage = "https://github.com/mrpteo/Error-Handling"
+	gem_info.homepage = "https://github.com/tophbeifong/Error-Handling/"
 end
--- lib/error-handling.rb
+++ lib/error-handling.rb
@@ -1,90 +1,85 @@
 class ErrorHandling
-	#global errors array
-	$stored_errors = Array.new()
 
-	def create(*error_arguments)
-		if error_arguments.length == 2
-			#push new error to global errors array.
+  $stored_errors = Array.new()
 
-			#get current date
-			time_date = Time.new
-			time = time_date.strftime('%Y-%m-%d')
-			$stored_errors.push([error_arguments[0], error_arguments[1], time])
-		else
-			#invalid number or parameters.
-			puts("Invalid error syntax, at least 2 parameters expected.")
-		end
-	end
+  def create(*error_arguments)
 
-	def display(display_errors_ammount)
-		if $stored_errors.length > 0
-			if display_errors_ammount.kind_of? Integer
-				if $stored_errors.length > display_errors_ammount
-					#display error
-					print($stored_errors[display_errors_ammount][0] + ": " + $stored_errors[display_errors_ammount][1])
-				else
-					puts("Invalid error syntax, invalid error number.")
-				end
-			elsif display_errors_ammount == 'last'
-				print($stored_errors[$stored_errors.length - 1][0] + ": " + $stored_errors[$stored_errors.length - 1][1])
+    return "Invalid error syntax, at least 2 parameters expected" if error_arguments.length != 2
 
-			else
-				if display_errors_ammount == 'all'
-					#loop through and disply the errors
-					$stored_errors.each{ |indiviual_error|
-						puts("[" + indiviual_error[2].to_s + "] " + indiviual_error[0] + ": " + indiviual_error[1])
-					}
-				end
+    #get current date
+    time = Time.new.strftime('%Y-%m-%d')
+    #store error
+		$stored_errors.push([error_arguments[0], error_arguments[1], time])
+  end
+
+  def display(display_errors_ammount)
+
+    #make sure errors are being stored
+    return puts "No errors are currently being stored." if $stored_errors.length < 0
+
+    #check data type
+    if display_errors_ammount.kind_of? Integer
+
+      return "Invalid error syntax, bad error number." if $stored_errors.length < display_errors_ammount
+
+      puts "[#{$stored_errors[display_errors_ammount][2]}] #{$stored_errors[display_errors_ammount][0]}: #{$stored_errors[display_errors_ammount][1]}"
+
+    elsif display_errors_ammount == 'last'
+
+      puts "[#{$stored_errors[$stored_errors.length - 1][2]}] #{$stored_errors[$stored_errors.length - 1][0]}: #{$stored_errors[$stored_errors.length - 1][1]}"
+
+    else
+
+      if display_errors_ammount == 'all'
+				#loop through and disply the errors
+				$stored_errors.each{ |indiviual_error|
+					puts "[#{indiviual_error[2]}] #{indiviual_error[0]}: #{indiviual_error[1]}"
+				}
 			end
-		else
-			puts("No errors are currently being stored,")
-		end
-	end
+    end
+  end
 
-	def count
-		#returns the amount of errors currently stored.
-		return $stored_errors.length
-	end
+  def count
+    return $stored_errors.length
+  end
 
-	def export(errors_export_name)
-		
-		#make sure 1 parameter is passed.
-		if !errors_export_name
-			puts("Invalid errors syntax, parameter expected \"filename\".")
-		end
+  def export(errors_export_name)
 
-		#create error log file
-		File.open(errors_export_name, "a") do |file|
+    #make sure filename is a string
+    puts "Invalid errors syntax, parameter passed must be a string." if !errors_export_name.kind_of? String
 
-			#loop through the errors putting them into the file
+    #create error log file
+    File.open(errors_export_name, "a") do |file|
+
+      #loop through the errors putting them into the file
 			for i in 0..$stored_errors.length - 1
-				error_message = "[" + $stored_errors[i][2] + "] " + $stored_errors[i][0] + ": " + $stored_errors[i][1]
+        error_message = "[#{$stored_errors[i][2]}] #{$stored_errors[i][0]}: #{$stored_errors[i][1]}"
 				file.puts(error_message)
 			end
-		end
-	end
+    end
+  end
 
-	def erase(*include_check)
-		if include_check[0] == 'check'
-			#provides more control
-			print("You are about to erase your errors, would you like to continue? [Y/N]")
-			
-			#loop to ensure y or n is entered.
-			while true
-				answer = gets.chop
-				if answer.upcase == 'Y'
-					$stored_errors.clear
-					break
-				elsif answer.upcase == 'N'
-					break
-				end	
-			end
-		else
-			#clear errors without check
-			$stored_errors.clear
-		end
-		return
-	end
+  def erase(*include_check)
+
+    if include_check[0] == 'check'
+    	#loop to ensure y or n is entered.
+    	loop do
+        print("You are about to erase your errors, would you like to continue? [Y/N]")
+    		answer = gets.chop.upcase
+
+        case answer
+        when 'Y' then
+          $stored_errors.clear
+          break
+        when 'N' then break
+        else puts "Invalid answer, try again."
+        end
+    	end
+    else
+    	#clear errors without check
+    	$stored_errors.clear
+    end
+  end
 
 	def load(*load_file)
 		#varify the file does inface exist
@@ -100,21 +95,17 @@ class ErrorHandling
 				$stored_errors.push([error_split[1].chop, error_split[2], error_split[0].slice(1..-1).chop])
 
 				#check for confirmation parameter
-				if load_file[1] and load_file[1] == 'confirm'
-					puts("External errors file loaded.")
-				end
+				puts "External errors file loaded." if load_file[1] == 'confirm'
 
 			end
 		else
-			puts("Load error: \"" + load_file[0] + "\" does not exist.")
+			return "Load error: \"#{load_file[0]}\" does not exist."
 		end
 	end
 
 	def search(error_type)
 		$stored_errors.each{ |error|
-			if error[0] == error_type
-				puts("[" + error[2] + "] " + error[0] + ": " + error[1])
-			end
+			puts "[" + error[2] + "] " + error[0] + ": " + error[1] if error[0] == error_type
 		}
 	end
 end
